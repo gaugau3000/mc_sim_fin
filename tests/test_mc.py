56d999172011c2f57d96dc1028e7b12e2842b576
Pialat <g.pialat@gmail.com>
2019-10-08 12:31:42 +0200

    fix test

--- tests/test_mc.py
+++ tests/test_mc.py
@@ -9,9 +9,9 @@ def test_start_equity_5000_ruin_equity_4000_1_year_test():
 
     df = pd.DataFrame({'result_dates': result_dates, 'result_amounts': result_amounts})
 
-    mc_sims_results = mc_analysis(df['result_dates'], df['result_amounts'], 5000, 4000, 1, 5000)
+    mc_sims_results = mc_analysis(df['result_dates'], df['result_amounts'], 5000, 4000, 1, 10000)
 
-    assert 0.15 < mc_sims_results['risk_of_ruin_percent'] < 0.18
+    assert 0.13 < mc_sims_results['risk_of_ruin_percent'] < 0.18
     assert 0.33 < round(mc_sims_results['med_drawdown_percent'], 2) < 0.37
     assert 1.80 < mc_sims_results['med_profit_percent'] < 2.00
     assert mc_sims_results['prob_profit_is_positive'] > 0.996
